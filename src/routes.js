917da473885d3c2843fcd8e25d65c5b9c882e77c
fernandovlima <fernandovlima.s@gmail.com>
2019-10-31 19:55:23 -0300

    add header and routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Cart from './pages/Cart';
