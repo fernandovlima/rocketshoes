82711071de4d00d43c7c91065896397d91698521
fernandovlima <fernandovlima.s@gmail.com>
2019-11-01 00:04:50 -0300

    grid products and add cart

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,101 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://lojavans.vteximg.com.br/arquivos/ids/281427-400-400/VN0A38DMJY3_001.jpg?v=637073671454900000"
+          alt="tenis"
+        />
+        <strong>Tênis Skate</strong>
+        <span>R$ 199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart color="#FFF" size={16} /> 3
+          </div>
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://lojavans.vteximg.com.br/arquivos/ids/281427-400-400/VN0A38DMJY3_001.jpg?v=637073671454900000"
+          alt="tenis"
+        />
+        <strong>Tênis Skate</strong>
+        <span>R$ 199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart color="#FFF" size={16} /> 3
+          </div>
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://lojavans.vteximg.com.br/arquivos/ids/281427-400-400/VN0A38DMJY3_001.jpg?v=637073671454900000"
+          alt="tenis"
+        />
+        <strong>Tênis Skate</strong>
+        <span>R$ 199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart color="#FFF" size={16} /> 3
+          </div>
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://lojavans.vteximg.com.br/arquivos/ids/281427-400-400/VN0A38DMJY3_001.jpg?v=637073671454900000"
+          alt="tenis"
+        />
+        <strong>Tênis Skate</strong>
+        <span>R$ 199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart color="#FFF" size={16} /> 3
+          </div>
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://lojavans.vteximg.com.br/arquivos/ids/281427-400-400/VN0A38DMJY3_001.jpg?v=637073671454900000"
+          alt="tenis"
+        />
+        <strong>Tênis Skate</strong>
+        <span>R$ 199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart color="#FFF" size={16} /> 3
+          </div>
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://lojavans.vteximg.com.br/arquivos/ids/281427-400-400/VN0A38DMJY3_001.jpg?v=637073671454900000"
+          alt="tenis"
+        />
+        <strong>Tênis Skate</strong>
+        <span>R$ 199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart color="#FFF" size={16} /> 3
+          </div>
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
