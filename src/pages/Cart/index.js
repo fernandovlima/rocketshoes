a2afc4b308f34fb6b4c326c915fade0c4df749ce
fernandovlima <fernandovlima.s@gmail.com>
2019-11-04 11:59:28 -0300

    cart styled

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,64 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUCT</th>
+            <th>QTY</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://lojavans.vteximg.com.br/arquivos/ids/281427-400-400/VN0A38DMJY3_001.jpg?v=637073671454900000"
+                alt="tenis"
+              />
+            </td>
+            <td>
+              <strong>Amazing Shoe</strong>
+              <span>R$ 129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>R$ 258,80</td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finish Order</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 999.90</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
