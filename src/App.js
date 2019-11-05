38bcb10230c6a9bbcb1ec0a0f37d83ffecc9921c
fernandovlima <fernandovlima.s@gmail.com>
2019-11-05 11:56:37 -0300

    stock validations and toast added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './config/ReactotronConfig';
+import { ToastContainer } from 'react-toastify';
 
 import GlobalStyle from './styles/global';
 
@@ -18,6 +19,7 @@ function App() {
         <Header />
         <GlobalStyle />
         <Routes />
+        <ToastContainer autoClose={3000} />
       </BrowserRouter>
     </Provider>
   );
